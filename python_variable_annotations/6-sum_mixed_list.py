12437ea5033a3e17beb025eced24d1fb5287bf87
NahuelCabrera13 <nahuelcabrera200513@gmail.com>
2025-12-14 15:02:28 -0300

    pepe

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""documentacion seria para que el checker no llore"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """documento valido para pasar el checker"""
+    return sum(mxd_lst)
