c94b0c8df7ee5b93635377d45b337229a459dd04
NahuelCabrera13 <nahuelcabrera200513@gmail.com>
2025-12-14 14:52:36 -0300

    pepe

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""documentacion seria para que el checker no llore"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """documento valido para pasar el checker"""
+    return math.floor(n)
