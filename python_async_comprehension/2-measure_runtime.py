2c91810cda59d007a95d4a06ac3398c6d92d7edc
NahuelCabrera13 <nahuelcabrera200513@gmail.com>
2025-12-14 15:29:11 -0300

    pepe

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -10,10 +10,10 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """documento valido para pasar el checker"""
     start = time.time()
-    await asyncio.gather(
+    await asyncio.gather(*[
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
-    )
+    ])
     return time.time() - start
