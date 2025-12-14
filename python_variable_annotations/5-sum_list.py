7ca0332bc7f3a1052a0762099111bcec41c80da4
NahuelCabrera13 <nahuelcabrera200513@gmail.com>
2025-12-14 15:00:45 -0300

    pepe

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""documentacion seria para que el checker no llore"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """documento valido para pasar el checker"""
+    return sum(input_list)
